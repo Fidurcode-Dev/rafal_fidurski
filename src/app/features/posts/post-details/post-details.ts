9ea7c2174ca6995529d183480fb8c13b5b403593
Fidurcode Development <dev@fidurcode.app>
2025-09-22 11:59:02 +0200

    Feat: Add post details: route, fetchig with httpResource.

--- src/app/features/posts/post-details/post-details.ts
+++ src/app/features/posts/post-details/post-details.ts
@@ -1,11 +1,26 @@
-import { Component } from '@angular/core';
+import {Component, inject, input, InputSignal, Signal} from '@angular/core';
+import { Loader } from '../../../shared/loader/loader';
+import {PostsService} from '../services/posts';
+import {HttpResourceRef} from '@angular/common/http';
+import {Post} from '../interfaces/post.model';
+import {toSignal} from '@angular/core/rxjs-interop';
+import {ActivatedRoute} from '@angular/router';
+import {map} from 'rxjs';
 
 @Component({
   selector: 'app-post-details',
-  imports: [],
+  standalone: true,
+  imports: [Loader],
   templateUrl: './post-details.html',
-  styleUrl: './post-details.css'
+  styleUrls: ['./post-details.css'],
 })
 export class PostDetails {
+  private route: ActivatedRoute = inject(ActivatedRoute);
+  postsService: PostsService = inject(PostsService);
+  postId: Signal<string> = toSignal(
+    this.route.paramMap.pipe(map(params => params.get('id') ?? '')),
+    { initialValue: '' }
+  );
 
+  postDetails: HttpResourceRef<Post | undefined> = this.postsService.get(this.postId);
 }
--- src/app/features/posts/services/posts.ts
+++ src/app/features/posts/services/posts.ts
@@ -1,5 +1,5 @@
-import {inject, Injectable} from '@angular/core';
-import {HttpClient} from '@angular/common/http';
+import {inject, Injectable, Signal} from '@angular/core';
+import {HttpClient, httpResource, HttpResourceRef, HttpResourceRequest} from '@angular/common/http';
 import {Observable} from 'rxjs';
 import {Post} from '../interfaces/post.model';
 
@@ -10,7 +10,16 @@ export class PostsService {
   private readonly http: HttpClient = inject(HttpClient);
   private readonly API_URL: string = 'https://jsonplaceholder.typicode.com/';
 
-  get(): Observable<Post[]> {
+  list(): Observable<Post[]> {
     return this.http.get<Post[]>(`${this.API_URL}posts`);
+  }
+
+  get(postId: Signal<string | undefined>): HttpResourceRef<Post | undefined> {
+    return httpResource<Post>(() => {
+      const id = postId();
+      return id
+        ? { url: `${this.API_URL}/posts/${id}` } as HttpResourceRequest
+        : undefined; // brak requestu, jeśli nie ma ID
+    });
   }
 }
